cbb756e5b2b2fffcf0ba97cd803173f313830ffc
vietanh <hoangvanvietanh@gmail.com>
2019-09-25 09:55:26 +0700

    design web

--- user_page/business/data.js
+++ user_page/business/data.js
@@ -1,5 +1,7 @@
 var Dia_chi_Dich_vu = "https://dv-webtracnghiem.herokuapp.com/"
 //var Dia_chi_Dich_vu = "http://localhost:1200/"
+var examList = Doc_Danh_sach_De_thi().Danh_sach_De_thi;
+var studentsList = Doc_Danh_sach_Sinh_vien().Danh_sach_Sinh_vien;
 
 function Doc_Danh_sach_De_thi() {
     var Du_lieu = {}
@@ -18,6 +20,19 @@ function Doc_Danh_sach_Sinh_vien() {
     var Du_lieu = {}
     var Xu_ly_HTTP = new XMLHttpRequest()
     var Tham_so = `Ma_so_Xu_ly=Doc_Danh_sach_Sinh_vien`
+    var Dia_chi_Xu_ly = `${Dia_chi_Dich_vu}?${Tham_so}`
+    Xu_ly_HTTP.open("POST", Dia_chi_Xu_ly, false)
+    Xu_ly_HTTP.send("")
+    var Chuoi_JSON = Xu_ly_HTTP.responseText
+    if (Chuoi_JSON != "")
+        Du_lieu = JSON.parse(Chuoi_JSON)
+    return Du_lieu
+}
+
+function Doc_Danh_sach_Sinh_vien_Thong_tin_Co_ban() {
+    var Du_lieu = {}
+    var Xu_ly_HTTP = new XMLHttpRequest()
+    var Tham_so = `Ma_so_Xu_ly=Doc_Danh_sach_Sinh_vien_Thong_tin_Co_ban`
     var Dia_chi_Xu_ly = `${Dia_chi_Dich_vu}?${Tham_so}`
     Xu_ly_HTTP.open("POST", Dia_chi_Xu_ly, false)
     Xu_ly_HTTP.send("")
